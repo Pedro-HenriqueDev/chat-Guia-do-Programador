1fdee327520fd5ead4847c4c1c6162032c9fdc36
Pedro-HenriqueDev <ph1305803@gmail.com>
2022-12-03 15:14:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on("connection",(socket) => {
+  socket.on("disconnect", (data) => {
+    console.log("Desconectado");
+  });
+
+  socket.on("msg", (data) => {
+    io.emit("showMsg", data)
+  });
+});
+
+
+app.set("view engine", "ejs");
+
+app.get('/', (req,res) => {
+  res.render('index')
+})
+
+
+
+
+http.listen(3000, () => {
+  console.log("RODANDO")
+});
